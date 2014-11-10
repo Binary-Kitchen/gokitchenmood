23b72c132a1d3143ac240d16de8cb473835f9a45
Philipp Neumann <pn@mtd-systems.de>
2014-11-10 20:04:11 +0100

    Blödes script rausgeschmissen bis mir was gutes einfällt

--- script/script.go
+++ script/script.go
@@ -1,13 +1,6 @@
 package script
 
-import (
-	"fmt"
-	"gokitchenmood/lampen"
-	"io/ioutil"
-	"time"
-
-	"github.com/stevedonovan/luar"
-)
+import "gokitchenmood/lampen"
 
 var p *lampen.Lampen = &lampen.Lampen{}
 var transfer = make(chan bool, 1)
@@ -28,50 +21,50 @@ func resetLamps() {
 }
 
 func repeatScript(content string) {
-	L := luar.Init()
-	defer L.Close()
-	// arbitrary Go functions can be registered
-	// to be callable from Lua
-	luar.Register(L, "", luar.Map{
-		"setLamps":   setLamps,
-		"sendLamps":  sendLamps,
-		"resetLamps": resetLamps,
-	})
-	//fmt.Println("Cont:", cont, "More:", more)
-	cont := true
-	more := true
-	for cont {
-		select {
-		case cont, more = <-transfer:
-			if !more {
-				cont = false
+	/*	L := luar.Init()
+		defer L.Close()
+		// arbitrary Go functions can be registered
+		// to be callable from Lua
+		luar.Register(L, "", luar.Map{
+			"setLamps":   setLamps,
+			"sendLamps":  sendLamps,
+			"resetLamps": resetLamps,
+		})
+		//fmt.Println("Cont:", cont, "More:", more)
+		cont := true
+		more := true
+		for cont {
+			select {
+			case cont, more = <-transfer:
+				if !more {
+					cont = false
+				}
+			default:
+				L.DoString(string(content))
+				//cont = <-transfer
+				//fmt.Println("Cont in:", cont)
 			}
-		default:
-			L.DoString(string(content))
-			//cont = <-transfer
-			//fmt.Println("Cont in:", cont)
 		}
-	}
-	repeatstopped = true
-	fmt.Println("Bye Bye")
-	//return nil
+		repeatstopped = true
+		fmt.Println("Bye Bye")
+		//return nil*/
 }
 
 func RunScript(script string) error {
-	transfer <- false
-	close(transfer)
-	for !repeatstopped {
-		time.Sleep(1 * time.Second)
-	}
-	contents, err := ioutil.ReadFile("uploaded/" + script)
-	if err != nil {
-		return err
-	}
-	//fmt.Println(string(contents))
-	transfer = make(chan bool, 1)
-	transfer <- true
-	repeatstopped = false
-	go repeatScript(string(contents))
-	//transfer <- true
+	/*	transfer <- false
+		close(transfer)
+		for !repeatstopped {
+			time.Sleep(1 * time.Second)
+		}
+		contents, err := ioutil.ReadFile("uploaded/" + script)
+		if err != nil {
+			return err
+		}
+		//fmt.Println(string(contents))
+		transfer = make(chan bool, 1)
+		transfer <- true
+		repeatstopped = false
+		go repeatScript(string(contents))
+		//transfer <- true*/
 	return nil
 }
